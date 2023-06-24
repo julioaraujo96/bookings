cea93abc69e2d406ae05b75ada548c69a9a3ea9b
julioaraujo96 <julioaraujo.bs@gmail.com>
2023-06-24 17:10:23 +0100

    configure vue tailwind date picker

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import App from './App.vue'
 import { createApp } from 'vue'
 import './style.css'
 import router from './router';
+import VueTailwindDatepicker from 'vue-tailwind-datepicker'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(router)
+.use(VueTailwindDatepicker)
+.mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
+
 export default {
   content: [
-    "./index.html",
+    "./index.html", 
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/vue-tailwind-datepicker/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "vtd-primary": colors.sky, // Light mode Datepicker color
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ]
 }
 
